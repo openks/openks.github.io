966579f9abcf618931fe148538bc9c644e11cd3a
openks <openks@foxmail.com>
2015-11-19 19:59:16 +0800

    Signed-off-by: openks <openks@foxmail.com>

---
+++ static/js/footer.js
@@ -0,0 +1,4 @@
+document.writeln('<div class="footer">')
+document.writeln('<div class="quicklink"><a href="###">用户中心</a>  |  <a href="###">联系方式</a>  |  <a href="###">关于我们</a>  |  <a href="###">青训对战</a>  |  <a href="###">活动</a>  |  <a href="###">未来之星</a></div>')
+document.writeln('<div>CopyRight©2015 上海聚运动文化传播有限公司     All Rigths Reserved 沪ICP备15015551号-1</div>')
+document.writeln('</div>')
